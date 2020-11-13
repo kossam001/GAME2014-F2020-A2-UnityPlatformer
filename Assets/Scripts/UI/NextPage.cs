7ab9eee49db5b7c96ab9ceb740b30fcc58156501
kossam001 <kossam001@gmail.com>
2020-11-13 14:01:09 -0500

    Added Health Indicators to Level1 and have the scenes transitioning to each other

---
+++ Assets/Scripts/UI/NextPage.cs
@@ -0,0 +1,37 @@
+﻿/* NextPage.cs
+ * 
+ * Samuel Ko
+ * 101168049
+ * Last Modified: 2020-09-26
+ * 
+ * Enables the next GameObject in a provided list while disabling the current GameObject.
+ * 
+ * 2020-09-26: Added script to demo the instruction screens.
+ */ 
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextPage : MonoBehaviour
+{
+    public List<GameObject> pages;
+    private int pageNum = 0;
+
+    public void TurnPage()
+    {
+        pages[pageNum].SetActive(false);
+
+        // Once the max page count is hit, go back to 0
+        if (pageNum == pages.Count - 1)
+        {
+            pageNum = 0;
+        }
+        else
+        {
+            pageNum++;
+        }
+
+        pages[pageNum].SetActive(true);
+    }
+}
