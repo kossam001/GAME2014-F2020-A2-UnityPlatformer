72268ce1e3b8d92284c793287a01bff83df6297f
kossam001 <kossam001@gmail.com>
2020-11-15 19:30:06 -0500

    Setting Up Player Movement
    Jumping does not work yet

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Joystick joystick;
+    public float horizontalSensitivity;
+    public float horizontalForce;
+    public float verticalSensitivity;
+    public float verticalForce;
+
+    [SerializeField]
+    private GameObject arm;
+    [SerializeField]
+    private GameObject weapon;
+    [SerializeField]
+    private GameObject upperBody;
+    [SerializeField]
+    private GameObject lowerBody;
+
+    private Animator armAnimator;
+    private Animator weaponAnimator;
+    private Animator topAnimator;
+    private Animator botAnimator;
+
+    private Rigidbody2D rigidbody2d;
+
+    private bool isGrounded;
+    private bool isJumping;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+
+        armAnimator = arm.GetComponent<Animator>();
+        weaponAnimator = weapon.GetComponent<Animator>();
+        topAnimator = upperBody.GetComponent<Animator>();
+        botAnimator = lowerBody.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (joystick.Horizontal > horizontalSensitivity)
+        {
+            // move right
+            rigidbody2d.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+
+            if (transform.localScale.x < 0)
+                transform.localScale = transform.localScale * new Vector2(-1, 1);
+
+            armAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            weaponAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            topAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            botAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+        }
+        else if (joystick.Horizontal < -horizontalSensitivity)
+        {
+            // move left
+            rigidbody2d.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+
+            if (transform.localScale.x > 0)
+                transform.localScale = transform.localScale * new Vector2(-1, 1);
+
+            armAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            weaponAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            topAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+            botAnimator.SetInteger("AnimState", (int)PlayerMovementState.RUN);
+        }
+        else
+        {
+            armAnimator.SetInteger("AnimState", (int)PlayerMovementState.IDLE);
+            weaponAnimator.SetInteger("AnimState", (int)PlayerMovementState.IDLE);
+            topAnimator.SetInteger("AnimState", (int)PlayerMovementState.IDLE);
+            botAnimator.SetInteger("AnimState", (int)PlayerMovementState.IDLE);
+        }
+
+        if ((joystick.Vertical > verticalSensitivity) && (!isJumping))
+        {
+            // jump
+            rigidbody2d.AddForce(Vector2.up * verticalForce);
+            isJumping = true;
+        }
+
+        if (isJumping)
+        {
+            botAnimator.SetBool("IsGrounded", true);
+        }
+        else if (isGrounded)
+        {
+            botAnimator.SetBool("IsGrounded", false);
+        }
+    }
+}
