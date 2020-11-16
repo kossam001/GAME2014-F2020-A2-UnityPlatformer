a836f0c054bafc3897bf406f411ceb20263ac3a0
kossam001 <kossam001@gmail.com>
2020-11-15 20:33:26 -0500

    Player Collision on Platform Only Checked When They Collide From Above

---
+++ Assets/Scripts/Player/DetectGround.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectGround : MonoBehaviour
+{
+    [SerializeField]
+    private CircleCollider2D cirCollider;
+
+    public bool isOnPlatform;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Platform")
+        {
+            Debug.Log("DAJDJIOJ");
+            cirCollider.enabled = true;
+            isOnPlatform = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        Debug.Log("DAJDJIOJ");
+        if (other.gameObject.tag == "Platform")
+        {
+            Debug.Log("DAJDJIOJ");
+            cirCollider.enabled = false;
+            isOnPlatform = false;
+        }
+    }
+}
