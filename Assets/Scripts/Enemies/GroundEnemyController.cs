dc030aeed627c0d1ef710c1c93d40bef29d335e3
kossam001 <kossam001@gmail.com>
2020-11-16 10:16:31 -0500

    GroundEnemy Moves on Platforms

--- Assets/Scripts/Enemies/GroundEnemyController.cs
+++ Assets/Scripts/Enemies/GroundEnemyController.cs
@@ -7,20 +7,45 @@ public class GroundEnemyController : MonoBehaviour
     public Transform lineEndpoint;
     public LayerMask layerMask;
 
+    private Rigidbody2D rigidbody2d;
+
+    public bool isPlatformEdge;
+    public float speed;
+    public float direction;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        direction = 1;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        
+        LookForPlatformEdge();
+        Move();
     }
 
     private void LookForPlatformEdge()
     {
-        Physics2D.Linecast(transform.position, lineEndpoint.position, layerMask);
+        isPlatformEdge = Physics2D.Linecast(transform.position, lineEndpoint.position, layerMask);
+
+        Debug.DrawLine(transform.position, lineEndpoint.position, Color.green);
+    }
+
+    private void Move()
+    {
+        if (isPlatformEdge)
+        {
+            rigidbody2d.AddForce(Vector2.left * speed * Time.deltaTime * direction);
+        }
+        else
+        {
+            transform.localScale = new Vector3(-transform.localScale.x, transform.localScale.y, transform.localScale.z);
+            direction *= -1;
+        }
+
+        rigidbody2d.velocity *= 0.90f;
     }
 }
