9a9217e0a4b1b1334c8ca03e56bbfad242f8a7ab
kossam001 <kossam001@gmail.com>
2020-11-16 12:32:24 -0500

    Added Enemy Knockback

---
+++ Assets/Scripts/Functional/Attack.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    public Vector2 knockbackForce = new Vector2(500,500);
+    public Vector2 direction = new Vector2(1, 1);
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        direction.x = transform.parent.transform.localScale.x;
+
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().AddForce(knockbackForce * direction);
+        }
+    }
+}
