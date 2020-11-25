c965d5541812ca67d6a783510d538df8cf00cbf8
kossam001 <kossam001@gmail.com>
2020-11-25 07:20:21 -0500

    Implemented Heart Skill
    Added a script to detect button hold down so that heal is a hold button.

---
+++ Assets/Scripts/Skills/Heal.cs
@@ -0,0 +1,37 @@
+﻿/* Heal.cs
+ * 
+ * Samuel Ko
+ * 101168049
+ * Last Modified: 2020-11-24
+ * 
+ * Reduces heart score by holding down the skill button.
+ * 
+ * 2020-11-24: Added this script.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Heal : MonoBehaviour, IUpdateSelectedHandler, IPointerDownHandler, IPointerUpHandler
+{
+    public bool isPressed;
+
+    // Start is called before the first frame update
+    public void OnUpdateSelected(BaseEventData data)
+    {
+        if (isPressed)
+        {
+            GameManager.Instance.UpdateHealth(-1, -1);
+        }
+    }
+    public void OnPointerDown(PointerEventData data)
+    {
+        isPressed = true;
+    }
+    public void OnPointerUp(PointerEventData data)
+    {
+        isPressed = false;
+    }
+}
---
+++ Assets/Scripts/Skills/Heart.cs
@@ -0,0 +1,52 @@
+﻿/* Heart.cs
+ * 
+ * Samuel Ko
+ * 101168049
+ * Last Modified: 2020-11-24
+ * 
+ * Binds healing skill to a button.  Does not really function as skill.
+ * Doesn't work with button on click event.
+ * 
+ * 2020-11-24: Added this script.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Heart", menuName = "Skills/Heart")]
+public class Heart : ISkill
+{
+    public override void Attach(GameObject _owner)
+    {
+        owner = _owner;
+    }
+
+    public override void DeductCost()
+    {
+    }
+
+    public override void Detach()
+    {
+    }
+
+    public override void Initialize()
+    {
+        SetCostLabel();
+    }
+
+    public override void SetCostLabel()
+    {
+        costLabel.text = "";
+    }
+
+    public override void SkillUse()
+    {
+    }
+
+    public override void AttachButton(Button button)
+    {
+        Heal healScript = button.gameObject.AddComponent<Heal>();
+    }
+}
